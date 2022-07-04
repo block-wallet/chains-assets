0065c34b3fceba64a762f0513dde655f21e83b3c
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2022-07-04 19:38:17 -0300

    exporting objects

---
+++ index.ts
@@ -0,0 +1,19 @@
+import {
+  ASSET_PLATFORMS_IDS_LIST,
+  AssetPlatformIdList,
+  CHAIN_LIST,
+  ChainListItem,
+  RATES_IDS_LIST,
+  RateIdList,
+  TOKENS_LIST,
+} from './public';
+
+export {
+  ASSET_PLATFORMS_IDS_LIST,
+  AssetPlatformIdList,
+  CHAIN_LIST,
+  ChainListItem,
+  RATES_IDS_LIST,
+  RateIdList,
+  TOKENS_LIST,
+};
---
+++ public/index.ts
@@ -0,0 +1,14 @@
+import {
+  ASSET_PLATFORMS_IDS_LIST,
+  AssetPlatformIdList,
+} from './asset-platforms-ids-list';
+export { ASSET_PLATFORMS_IDS_LIST, AssetPlatformIdList };
+
+import { CHAIN_LIST, ChainListItem } from './chain-list';
+export { CHAIN_LIST, ChainListItem };
+
+import { RATES_IDS_LIST, RateIdList } from './rates-ids-list';
+export { RATES_IDS_LIST, RateIdList };
+
+import { TOKENS_LIST } from './token-list';
+export { TOKENS_LIST };
